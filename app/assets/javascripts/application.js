bf4f74c7454677ec93a602b1c2241098f9ee0536
snayrouz <samuel.nayrouz@gmail.com>
2018-06-26 20:31:45 -0600

    Add requirements to application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require moment
+//= require fullcalendar
+//= require daterangepicker
 //= require_tree .
