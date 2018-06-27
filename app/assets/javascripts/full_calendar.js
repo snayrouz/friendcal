0cff7a3444ce287cd2af54d31dacace6a1e07f43
snayrouz <samuel.nayrouz@gmail.com>
2018-06-26 21:02:17 -0600

    Add headers to calendar

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -2,7 +2,13 @@ var initialize_calendar;
 initialize_calendar = function(){
     $('.calendar').each(function(){
         var calendar = $(this);
-        calendar.fullCalendar({});
+        calendar.fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+        });
     })
 };
 $(document).on('turbolinks:load', initialize_calendar);
