851b41c37fc2a92393c57cd28068bc474b1f7278
snayrouz <samuel.nayrouz@gmail.com>
2018-06-27 14:25:58 -0600

    Add index view for events jbuilder

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -12,7 +12,7 @@ initialize_calendar = function(){
             selectHelper: true,
             editable: true,
             eventLimit: true,
-
+            events: '/events.json',
             select: function(start, end) {
                 $.getScript('/events/new', function() {
                     $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
