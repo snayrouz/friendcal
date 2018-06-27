7d89c095b646e506bde9c4e9ea08573f0b7a30ea
snayrouz <samuel.nayrouz@gmail.com>
2018-06-27 09:49:47 -0600

    Add create, new, event params and set event before action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,42 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy ]
+
+  def index
+
+  end
+
   def new
     @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title,:date_range, :start, :end, :color)
+    end
+
+    def set_event
+      @event = Event.find(params[:id])
+    end
 end
