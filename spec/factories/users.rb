ce16993bb58b20b78b7e7c98fd94c8523e27c4fa
snayrouz <samuel.nayrouz@gmail.com>
2018-07-08 12:50:24 -0600

    Add users model and migration

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    first_name "MyString"
+    last_name "MyString"
+    email "MyString"
+    password_digest "MyString"
+    address "MyString"
+    provider "MyString"
+    uid "MyString"
+    oauth_token "MyString"
+    oauth_expiration "2018-07-08 12:50:05"
+    verfication_code "MyString"
+    phone "MyString"
+  end
+end
