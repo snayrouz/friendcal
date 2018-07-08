af89ffc8cf72259ecf50baf84067a4cb401537b2
snayrouz <samuel.nayrouz@gmail.com>
2018-07-08 12:54:03 -0600

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :first_name, :last_name, :password_digest, presence: true
+  validates :phone, presence: true, on: :create, unless: :oauth_login
+  validates :email, presence: true, on: :create, uniqueness: true, unless: :oauth_login
+
+  def oauth_login
+    oauth_token.present?
+  end
 end
