4614aab2fedaa7143d8facce2af741bb343bd696
snayrouz <samuel.nayrouz@gmail.com>
2018-07-18 17:51:06 -0600

    Add message_sender, confirmation_sender and code generator models

---
+++ app/models/confirmation_sender.rb
@@ -0,0 +1,9 @@
+class ConfirmationSender
+
+  def self.send_confirmation(user)
+    verification_code = CodeGenerator.generate
+    user.update(verification_code: verification_code)
+    MessageSender.send_code(user.phone, verification_code)
+  end
+
+end
