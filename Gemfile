1e9cfc0b63e0ef4b61d6dcfddd76678114fb97b1
snayrouz <samuel.nayrouz@gmail.com>
2018-07-18 17:35:00 -0600

    Add google and twitter omniauth middleware

--- Gemfile
+++ Gemfile
@@ -44,7 +44,7 @@ gem 'figaro'
 gem 'omniauth'
 gem 'omniauth-twitter'
 gem 'active_model_serializers'
-
+gem 'omniauth-google-oauth2'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'main#index'
 
+  get '/auth/twitter', to: 'sessions#create', as: 'twitter_login'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+
   get 'signout', to: 'sessions#destroy', as: 'signout'
   get 'login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
