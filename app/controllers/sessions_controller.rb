3c45f9bf1a4d5bb1075a3a4eef1eca42b6ec7e6d
snayrouz <samuel.nayrouz@gmail.com>
2018-07-18 13:13:29 -0600

    Add skeleton actions to sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def verify_user
+
+    end
+
+    def login_successful
+
+    end
+end
