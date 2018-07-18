42bd3e849bafaf65fb1e4b46269b5813d295b844
snayrouz <samuel.nayrouz@gmail.com>
2018-07-18 13:23:10 -0600

    Add sessions, users, root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'main#index'
+
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#delete'
+
+  resources :users, only: [:new, :create, :edit, :update]
+  resources :visitors, only: :index
   resources :events
-  root 'visitors#index'
 end
